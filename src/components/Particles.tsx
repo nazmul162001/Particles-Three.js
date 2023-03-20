cb41e2248f9abebbd141b45ab30b2d698a18c07d
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-03-20 12:01:42 +0600

    add particles & load particles texture

--- src/components/Particles.tsx
+++ src/components/Particles.tsx
@@ -23,16 +23,32 @@ const Particles = () => {
      * Textures
      */
     const textureLoader = new THREE.TextureLoader()
+    const particleTexture = textureLoader.load('/textures/particles/2.png')
 
     /**
      * Particles
      */
     // Geometry
-    const particlesGeometry = new THREE.SphereGeometry(1, 32, 32) // radius, subDivisionWidth, subDivisionHeight
+    const particlesGeometry = new THREE.BufferGeometry()
+    const count = 5000
+
+    const positions = new Float32Array(count * 3)
+
+    for (let i = 0; i < count * 3; i++) {
+      positions[i] = (Math.random() - 0.5) * 10
+    }
+
+    particlesGeometry.setAttribute(
+      'position',
+      new THREE.BufferAttribute(positions, 3)
+    )
+
     // Material
     const particlesMaterial = new THREE.PointsMaterial()
-    particlesMaterial.size = 0.02
+    particlesMaterial.size = 0.1
     particlesMaterial.sizeAttenuation = true
+    particlesMaterial.color = new THREE.Color('#ff88cc')
+    particlesMaterial.map = particleTexture
 
     // Points
     const particles = new THREE.Points(particlesGeometry, particlesMaterial)

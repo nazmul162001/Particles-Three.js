3350e5f6a9873c8e5a9acb7e9906fe059f819ceb
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-03-20 12:32:15 +0600

    added particles material & alphaTest

--- src/components/Particles.tsx
+++ src/components/Particles.tsx
@@ -48,11 +48,21 @@ const Particles = () => {
     particlesMaterial.size = 0.1
     particlesMaterial.sizeAttenuation = true
     particlesMaterial.color = new THREE.Color('#ff88cc')
-    particlesMaterial.map = particleTexture
+    particlesMaterial.transparent = true
+    particlesMaterial.alphaMap = particleTexture
+    // particlesMaterial.alphaTest = 0.001
+    // particlesMaterial.depthTest = false
+    particlesMaterial.depthWrite = false
 
     // Points
     const particles = new THREE.Points(particlesGeometry, particlesMaterial)
     scene.add(particles)
+
+    const cube = new THREE.Mesh(
+      new THREE.BoxGeometry(),
+      new THREE.MeshBasicMaterial()
+    )
+    scene.add(cube)
 
     /**
      * Sizes
